2f9be866a57a6d2cc2a32c55508b13d610d38037
Tim Quayle <37129455+timquayle@users.noreply.github.com>
2018-07-20 12:41:04 -0700

    Update data.service.ts

--- ninja-gold/src/app/data.service.ts
+++ ninja-gold/src/app/data.service.ts
@@ -1,18 +1,19 @@
-import { Injectable } from '@angular/core';
-
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
-goldcount=0;
-  constructor() {  }
+  goldcount: BehaviorSubject<number> = new BehaviorSubject(0);
+constructor() {  }
   
   makeNum(min: number, max: number){
    const num =  Math.round(Math.random() * (max - min) + min);
-   //this.goldcount = this.goldcount + num;
    console.log("NUM",num);
-   console.log("GC ",this.goldcount);
+   //console.log("GC ",this.goldcount);
    return num;
   }
- 
+  addGold(newgold: any): void{
+    const tempData = this.goldcount.getValue();
+    this.goldcount.next(newgold);
+  }
 }
+
