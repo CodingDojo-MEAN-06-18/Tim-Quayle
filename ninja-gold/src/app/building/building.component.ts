4abe30fcd380903e21d5d3697d07cbfda04d93f5
Tim Quayle <37129455+timquayle@users.noreply.github.com>
2018-07-20 12:36:29 -0700

    Update building.component.ts

--- ninja-gold/src/app/building/building.component.ts
+++ ninja-gold/src/app/building/building.component.ts
@@ -13,11 +13,16 @@ export class BuildingComponent implements OnInit {
   @Output() passUpgold = new EventEmitter();
   gobj = {gcount: 0,
           source: ''};
-  gcnt=0;
+  gcnt: number=0;
+  lgoldcount: number =  0;
   constructor(private _dataService: DataService) { }
  
   onClick(){
+
     this.gcnt= this._dataService.makeNum(this.min, this.max)
+    this.lgoldcount = this.lgoldcount + this.gcnt;
+    this._dataService.addGold(this.lgoldcount);
+    console.log("GoldCOunt:",this.lgoldcount)
     console.log("Clicked!")
     this.gobj = {
       gcount: this.gcnt,
@@ -27,7 +32,8 @@ console.log("CHILD gold",this.gobj.gcount);
 console.log("CHILD gold",this.gobj.source);
                 this.passUpgold.emit(this.gobj);
 }
-  ngOnInit() {
-  }
+  ngOnInit(){ this._dataService.goldcount.subscribe(
+    (goldcount) => {this.lgoldcount = goldcount}
+  )}
 
 }
