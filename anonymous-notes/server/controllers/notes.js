db16226c4e7b5be87c75ab71fdf51faea97d8c74
Tim Quayle <37129455+timquayle@users.noreply.github.com>
2018-07-21 15:48:59 -0700

    Update notes.js

--- anonymous-notes/server/controllers/notes.js
+++ anonymous-notes/server/controllers/notes.js
@@ -4,25 +4,30 @@ module.exports = {
 
 
    
-notesadd: function(req, res) {
-     var bid = req.params.foo
-     console.log("IN NOTESADDmnbmjbm.bk.jbkjlhlkhj")
-    
+ notesadd: function(req, res) {
+     //const note = req.body;
+     console.log("passed note:",req.body.note);
+     const note = new Notes({note: req.body.note});
+     note.save();
 },
  
 notesget: function(req,res) {
     Notes.find({},function(err, notes) {
-        if(err) {
+         //console.log("REQueST:",req);
+         //console.log("RESPONSE:",res);
+      if(err) {
             console.log('something went wrong');
-            console.log(notes.errors);
+            console.log(err);
           }
           else {
-            console.log("notes get is working")
+            console.log("notes get is working!!!!!!!!")
             console.log(notes);
             res.json(notes);
-            
-          }
-                                           })
+                
+           // })
+          
+                }
+          })
                           },
            
 
