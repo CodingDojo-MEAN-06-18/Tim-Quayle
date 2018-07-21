a5800f250ca070a8075a0cb8d953d1088f30c90f
Tim Quayle <37129455+timquayle@users.noreply.github.com>
2018-07-21 15:53:33 -0700

    Update app.component.ts

--- anonymous-notes/anonymous-notes/src/app/app.component.ts
+++ anonymous-notes/anonymous-notes/src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+ newnotes=[];
   title = 'app';
+  newNotes(eventData){
+ console.log("NEWQUOTETOBEPOSTED ",eventData)
+  this.newnotes=eventData;
+}
+
+
+
+
 }
