cdc33c0f2a5fa3f68088cfed1ffb73f043ff7834
Tim Quayle <37129455+timquayle@users.noreply.github.com>
2018-07-21 15:50:54 -0700

    Update anon.js

--- anonymous-notes/server/models/anon.js
+++ anonymous-notes/server/models/anon.js
@@ -3,5 +3,5 @@ var AnonNoteSchema = new mongoose.Schema({
  note: {type: String, required: [true, "Note is required"]},
 },{timestamps: true })
 mongoose.model('Notes', AnonNoteSchema); 
-var Badger = mongoose.model('Notes') 
+var Notes = mongoose.model('Notes') 
 module.exports = mongoose.model("Notes");
